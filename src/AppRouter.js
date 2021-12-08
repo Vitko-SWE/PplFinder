f05cde3c0ee93ed4f4d5e0b43909b23a48f5bfa9
Vitalii Kovtunenko <vitalykovtunenko92@gmail.com>
2021-12-08 16:13:07 +0200

    new patch

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -8,7 +8,7 @@ import Favorites from "pages/Favorites/Favorites";
 const AppRouter = () => {
   return (
     <ThemeProvider>
-      <Router basename={process.env.PUBLIC_URL}>
+      <Router basename={window.location.pathname || ''}>
         <NavBar />
         <Switch>
           <Route exact path="/" component={Home} />
