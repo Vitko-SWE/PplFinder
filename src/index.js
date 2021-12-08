54214723508504b3c7533172cc8ce057f0d3ec09
Vitalii Kovtunenko <vitalykovtunenko92@gmail.com>
2021-12-08 15:11:38 +0200

    new patch

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import AppRouter from "AppRouter";
 
-ReactDOM.render(<AppRouter />, document.querySelector("#root"));
+ReactDOM.render(<AppRouter />, document.getElementById("root"));
